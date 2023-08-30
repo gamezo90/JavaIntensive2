65b9472129ac0e5815b39927597bda51d23056eb
Nick <gamezo90@mail.ru>
2023-08-30 19:47:31 +0300

    1

--- src/main/java/task1/TradingHistory.java
+++ src/main/java/task1/TradingHistory.java
@@ -5,25 +5,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TradingHistory {
-
-
     public static void main(String[] args) {
+        Computer.createSomeComputers();
+        User.createSomeUsers();
 
-        Computer computer = new Computer();
-        computer.setId(1);
-        computer.setManufacturer("fsdf");
-        computer.setModel("fsdaf");
-        computer.setPrice(BigDecimal.valueOf(34));
 
-        User user = new User(423, 43, "fsdf", "fsdf");
-
-        Discount discount = new Discount(534, 5353f);
-
-        Buying buying =new Buying(user, computer, discount);
-
-        Selling selling = new Selling();
-      //  addDeal(buying, selling).toArray();
-        System.out.println(buying);
     }
     public static  ArrayList addDeal(Buying buying, Selling selling) {
 
